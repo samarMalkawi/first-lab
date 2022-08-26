bd5d5baf019cc7f613d259533390b4f488dea20f
samarMalkawi <summarmalkawi@gmail.com>
2022-08-26 16:31:49 +0300

    My assignment 1

--- main.dart
+++ main.dart
@@ -1,5 +1,34 @@
-void main(){
-  String myName = "Samar Malkawi";
-
-  print(myName);
+void main() {
+  Map myInformation = {
+    "my name": "samar",
+    "age": 24,
+    "family memebers": [
+      {
+        "father": "Ramzi",
+        "Mother": "soher",
+        "Brothers": ["Hamza", "amer"],
+        "sisters": ["Aya", "sabaa"]
+      }
+    ],
+    "Education info": {
+      "University": "Yu",
+      "Faculty": "IT",
+      "major": "computer siance",
+      "Graduation year": 2022
+    }
+  };
+  print(myInformation);
+  print(myInformation.keys);
+  print(myInformation.values);
+  print(myInformation.isEmpty);
+  print(myInformation.length);
+  myInformation
+      .addAll({"favuoirt colore": "green", "Nationality": "Jordanian"});
+  print(myInformation);
+  myInformation.remove("favuoirt colore");
+  print(myInformation);
+  myInformation.forEach((k, v) {
+    print(k.toString().toUpperCase());
+    print(v.toString().length);
+  });
 }
